97dbff057ea133a4251e2527f3986073bcef3f69
atish0117 <atishr334@gmail.com>
2024-08-31 12:52:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+function generateQR() {
+
+    if (qrText.value==="") {
+            qrText.classList.add('error')
+            setTimeout(() => {
+                    qrText.classList.remove('error')
+                }, 1000);
+        }
+        else{
+            qrImage.src =  "  https://quickchart.io/qr?text="+qrText.value+"&centerImageUrl=https://cdn-icons-png.flaticon.com/512/1389/1389234.png"
+            // qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="+qrText.value ;
+            imgBox.classList.add('show-img');
+        }
+        qrText.value=""
+}
